c99515d0073b26cc3792bce6b15106e94a89b6b8
SmileTTP <ibtissamlazraf@gmail.com>
2025-11-19 16:00:12 +0100

    index page done

--- app/index.tsx
+++ app/index.tsx
@@ -5,17 +5,18 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 export default function Index() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1, }}>
-        <ImageBackground source={require('@/assets/images/backgroundimage.png')} resizeMode="cover"  style={{ flex: 1, }}>
-        <View className="flex-1 justify-center items-center">
+      <SafeAreaView className="flex-1">
+        <ImageBackground source={require('@/assets/images/backgroundimage.png')} resizeMode="cover" className="flex-1">
+        <View className="flex-1 justify-end pb-20 items-center gap-4">
           <Image source={require('@/assets/images/whitecarrot.png')} className="w-[48px] h-[56px]"/>
-          <Text className="text-5xl font-bold text-white">Welcome to our store</Text>
+          <Text className="text-5xl font-bold text-white text-center">Welcome</Text>
+          <Text className="text-5xl font-bold text-white text-center">to our store</Text>
           <View>
             <Text className="text-base text-[#FCFCFCB2] font-medium">Ger your groceries in as fast as one hour</Text>
           </View>
-          <TouchableOpacity className="bg-[#53B175] rounded-md">
+          <TouchableOpacity className="bg-[#53B175] rounded-xl h-[67px] w-[353px] px-6 py-6">
             <Link href={'/signin'}>
-            <Text className="text-lg">Get Started</Text>
+            <Text className="text-lg text-white text-center font-medium ">Get Started</Text>
             </Link>
           </TouchableOpacity>
         </View>
