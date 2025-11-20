0641bdcf3b7611be0acb2559acc865a35bb8260f
SmileTTP <ibtissamlazraf@gmail.com>
2025-11-20 20:26:43 +0100

    .

--- app/(home)/explore.tsx
+++ app/(home)/explore.tsx
@@ -1,10 +1,35 @@
+import ProductsCard from "@/components/common/productsCard";
+import { FILTERS } from "@/constants/data";
+import { Feather } from "@expo/vector-icons";
 import { View, Text, TextInput, Image, ScrollView, Dimensions, TouchableHighlight} from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
 const Explore = () => {
     return (
         <SafeAreaProvider>
-            <SafeAreaView className="flex-1 bg-white">
-                
+            <SafeAreaView className="flex-1 bg-white gap-4">
+            <View className="mt-4 flex-1">
+            <View className="mt-4 items-center">
+                <Text className="font-bold text-2xl">Find Products</Text>
+            </View>
+            <View className="px-4 mb-4">
+            <View className="mt-4 flex-row items-center justify-between px-4 p-2 border-gray-100 border rounded-xl h-[51px]">
+                <Feather name="search" size={24} color="gray" />
+                <TextInput keyboardType="default" className="w-[330px] flex-1" />
+            </View>
+            </View>
+            <View className="flex-1 mb-4">
+            <ScrollView>
+            <View className="flex flex-row flex-wrap p-2">
+                {FILTERS.map((filter, index) => (
+                    <View key={index} className="w-1/2 p-1">
+                    <ProductsCard name={filter.name} image={filter.image}/>
+                    </View>
+                ))}
+            </View>
+            </ScrollView>
+            </View>
+            </View>
             </SafeAreaView>
         </SafeAreaProvider>
     )
--- components/common/productsCard.tsx
+++ components/common/productsCard.tsx
@@ -5,10 +5,10 @@ const ProductsCard: React.FC<FitlersProps> = ({ name, image }) => {
     return (
         <View className="w-[174.5px] h-[189.10609436035156px] border border-gray-200 rounded-lg items-center">
             <View className="mt-4 items-center">
-                <Image source={image} className="w-[99.88697814941445px] h-[79.43215179443389px]" resizeMode="cover"/>
+                <Image source={image} className="w-[99px] h-[79px] mt-4" resizeMode="cover"/>
             </View>
-            <View className="mt-4">
-                <Text className="px-4 mt-4 font-bold text-lg">
+            <View className="mt-4 absolute bottom-4 items-center px-4">
+                <Text className="font-bold text-lg">
                 {name}
                 </Text>
             </View>
